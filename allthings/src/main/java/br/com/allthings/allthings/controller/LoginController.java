6e767b4a332a4dbde36e290859cee01e810817a6
Nawanny <nawannysoares@gmail.com>
2026-09-22 20:49:29 -0300

    fazendo os logins

--- allthings/src/main/java/br/com/allthings/allthings/controller/LoginController.java
+++ allthings/src/main/java/br/com/allthings/allthings/controller/LoginController.java
@@ -1,7 +1,10 @@
 package br.com.allthings.allthings.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller 
 public class LoginController {
     
      @GetMapping("/login")
--- allthings/src/main/java/br/com/allthings/allthings/security/UserDetailsServiceImpl.java
+++ allthings/src/main/java/br/com/allthings/allthings/security/UserDetailsServiceImpl.java
@@ -1,25 +1,25 @@
-// package br.com.allthings.allthings.security;
+package br.com.allthings.allthings.security;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-// import br.com.allthings.allthings.entity.Usuario;
-// import br.com.allthings.allthings.repository.UsuarioRepository;
+import br.com.allthings.allthings.entity.Usuario;
+import br.com.allthings.allthings.repository.UsuarioRepository;
 
-// @Service
-// public class UserDetailsServiceImpl implements UserDetailsService {
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
 
-//     @Autowired
-//     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-//     @Override
-//     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-//         Usuario usuario = usuarioRepository.findByLoginUsuario(login)
-//             .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + login));
-//         return new UserDetailsImpl(usuario);
-//     }
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByLoginUsuario(login)
+            .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + login));
+        return new UserDetailsImpl(usuario);
+    }
 
-// }
+}
